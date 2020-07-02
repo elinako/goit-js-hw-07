0bb75583f1449191ea1516a0279a3a0b48a65e80
Elina <elisonfog@gmail.com>
2020-07-02 23:32:07 +0300

    added new version task-02

--- task-02.js
+++ task-02.js
@@ -9,19 +9,22 @@ const ingredients = [
 
 const ul = document.querySelector("ul#ingredients");
 
-const createList = function (array) {
-  const div = document.createElement("div");
-  for (let i = 0; i < array.length; i += 1) {
-    const li = document.createElement("li");
-    li.textContent = `${array[i]}`;
-    div.appendChild(li);
-  }
+// const createList = function (array) {
+//   const div = document.createElement("div");
+//   for (let i = 0; i < array.length; i += 1) {
+//     const li = document.createElement("li");
+//     li.textContent = `${array[i]}`;
+//   }
+//   ul.appendChild(div);
+//   return ul;
+// };
 
-  return div;
-};
+function crt(element) {
+  const li = document.createElement("li");
+  li.textContent = `${element}`;
+  return li;
+}
 
-const list = createList(ingredients);
+const list = ingredients.forEach((item) => crt(item));
 
 ul.append(list);
-
-console.log(ul);
