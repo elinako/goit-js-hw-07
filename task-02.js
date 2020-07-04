b61ae9a629b710a853e2b76d24449fe8621c7290
Elina <elisonfog@gmail.com>
2020-07-04 11:16:22 +0300

    added task-02

--- task-02.js
+++ task-02.js
@@ -9,24 +9,14 @@ const ingredients = [
 
 const ul = document.querySelector("ul#ingredients");
 
-// const createList = function (array) {
-//   const div = document.createElement("div");
-//   for (let i = 0; i < array.length; i += 1) {
-//     const li = document.createElement("li");
-//     li.textContent = `${array[i]}`;
-//   }
-//   ul.appendChild(div);
-//   return ul;
-// };
+function crt(element) {
+  const li = document.createElement("li");
+  li.textContent = `${element}`;
+  return li;
+}
 
-// function crt(element) {
-//   const li = document.createElement("li");
-//   li.textContent = `${element}`;
-//   return li;
-// }
+const list = ingredients.map((item) => {
+  return crt(item);
+});
 
-const list = ingredients.reduce((acc, item) => acc + `<li>${item}</li>`, "");
-
-console.log(list);
-
-ul.insertAdjacentHTML("afterbegin", list);
+ul.append(...list);
